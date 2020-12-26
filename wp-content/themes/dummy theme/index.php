4563b311f102b1949c3f60a60ad7fa13071c9f16
Khoi Hoang <hello@koicha.dev>
2020-12-26 23:49:14 +0100

    Add: :sparkles: helpers.php and prefix functions for posts
    The placeholder as an example when we are rendering

--- wp-content/themes/dummy theme/index.php
+++ wp-content/themes/dummy theme/index.php
@@ -10,24 +10,13 @@
             </a>
         </h2>
         <div>
-            <p>
-                Posted on 
-                <a href="<?php echo get_permalink(); ?>">
-                    <time datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date(); ?></time> 
-                </a>
-                By 
-                <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
-                    <?php echo get_the_author(); ?>
-                </a>
-            </p> 
+           <?php dummy_theme_post_meta(); ?>
         </div>
         
         <div>
             <?php the_excerpt(); ?>
         </div>
-        <a href="<?php echo get_the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-            Read More <span class="u-screen-reader-text">About <?php the_title(); ?></span>
-        </a>
+            <?php dummy_theme_read_more(); ?>
     <?php endwhile;?>
     <?php the_posts_pagination(); ?>
 <?php else : ?>

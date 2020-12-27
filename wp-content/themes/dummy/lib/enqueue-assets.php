ab8f49ddc76b42ff7b9302c48b1b4e214e5c81e3
Khoi Hoang <hello@koicha.dev>
2020-12-27 14:30:05 +0100

    Add: :sparkles: Enqueuing stylesheet for admin dashbard

--- wp-content/themes/dummy/lib/enqueue-assets.php
+++ wp-content/themes/dummy/lib/enqueue-assets.php
@@ -1,6 +1,12 @@
 <?php 
-    function dummy__theme_assets() {
-        wp_enqueue_style('dummy-theme-stylesheet', get_template_directory_uri() . '/dist/assets/css/bundle.css', [], 'all');
+    function dummy_theme_assets() {
+        wp_enqueue_style('dummy-theme-stylesheet', get_template_directory_uri() . '/dist/assets/css/bundle.css', [], '1.0.0', 'all');
     }
 
-    add_action('wp_enqueue_scripts', 'dummy__theme_assets');
+    function dummy_theme_admin_assets() {
+        wp_enqueue_style('dummy-admin-stylesheet', get_template_directory_uri() . '/dist/assets/css/admin.css', [], '1.0.0', 'all');
+    }
+
+    add_action('wp_enqueue_scripts', 'dummy_theme_assets');
+    add_action('admin_enqueue_scripts', 'dummy_theme_admin_assets');
+

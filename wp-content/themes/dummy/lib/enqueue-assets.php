412a28ef8b14e7d5423db61146dd108a8b42f2b3
Khoi Hoang <hello@koicha.dev>
2020-12-27 16:15:10 +0100

    Add: :sparkles: enqueue for admin dashboard JavaScript

--- wp-content/themes/dummy/lib/enqueue-assets.php
+++ wp-content/themes/dummy/lib/enqueue-assets.php
@@ -1,11 +1,14 @@
 <?php 
     function dummy_theme_assets() {
         wp_enqueue_style('dummy-theme-stylesheet', get_template_directory_uri() . '/dist/assets/css/bundle.css', [], '1.0.0', 'all');
-        wp_enqueue_script('dummy-theme-scripts', get_template_directory_uri() . '/dist/assets/js/bundle.js', [], true);
+        wp_enqueue_script('dummy-theme-scripts', get_template_directory_uri() . '/dist/assets/js/bundle.js', [], '1.0.0', true);
+        
     }
 
     function dummy_theme_admin_assets() {
         wp_enqueue_style('dummy-admin-stylesheet', get_template_directory_uri() . '/dist/assets/css/admin.css', [], '1.0.0', 'all');
+        wp_enqueue_script('dummy-theme-admin-scripts', get_template_directory_uri() . '/dist/assets/js/admin.js', [], '1.0.0', true);
+
     }
 
     add_action('wp_enqueue_scripts', 'dummy_theme_assets');

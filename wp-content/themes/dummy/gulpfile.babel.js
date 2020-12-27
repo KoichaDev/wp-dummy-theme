f76da18c9520ddf680c78d93316af5d5b65b44af
Khoi Hoang <hello@koicha.dev>
2020-12-27 14:23:58 +0100

    Add: :sparkles: sourcemaps for stylesheets
    The configuration how gulp will use the sourcemap of stylesheets during development

--- wp-content/themes/dummy/gulpfile.babel.js
+++ wp-content/themes/dummy/gulpfile.babel.js
@@ -2,16 +2,18 @@ import gulp from 'gulp';
 import yargs from 'yargs';
 import sass from 'gulp-sass';
 import cleanCSS from 'gulp-clean-css';
-import gulpIf from 'gulp-if';
+import gulpif from 'gulp-if';
+import sourcemaps from 'gulp-sourcemaps';
 
 const PRODUCTION = yargs.argv.prod;
 
 export const styles = () => {
   return gulp
     .src('src/assets/scss/bundle.scss')
+    .pipe(gulpif(!PRODUCTION, sourcemaps.init()))
     .pipe(sass().on('error', sass.logError))
     .pipe(
-      gulpIf(
+      gulpif(
         // We want to run the production environment if it's true
         PRODUCTION,
         cleanCSS({
@@ -19,7 +21,8 @@ export const styles = () => {
         })
       )
     )
-    .pipe(gulp.dest('dist/asset/css'));
+    .pipe(gulpif(!PRODUCTION, sourcemaps.write()))
+    .pipe(gulp.dest('dist/assets/css'));
 };
 
 // export default hello;

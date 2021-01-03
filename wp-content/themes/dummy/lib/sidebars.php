6fc8b6658ca52f4ce32db85d06df2be4513a7ddf
Khoi Hoang <hello@koicha.dev>
2021-01-03 17:54:43 +0100

    Add: :sparkles: Widget will be displayed on the admin dashboard
    It will only display on the admin dashboard. Not on the front-end end
    yet. That is where on the next commit we will fix it

---
+++ wp-content/themes/dummy/lib/sidebars.php
@@ -0,0 +1,16 @@
+<?php 
+
+function _theme_name_sidebar_widgets() {
+    register_sidebar([
+        'id' => 'primary-sidebar',
+        'name' => esc_html__('Primary Sidebar', '_theme_name'),
+        'description' => esc_html__('This sidebar appears in the blog post page', '_theme_name'),
+        // %1$s WP will create automatically an ID and %2$s WP will create a placeholder
+        'before_widget' => '<section id="%1$s" class="c-sidebar-widget u-margin-bottom-20 %2$s">', // This will be any HTMl print before the widget
+        'after_widget' => '</section>',
+        'before_title' => '<h5>',
+        'after_title' => '</h5>'
+    ]);
+}
+
+add_action('widgets_init', '_theme_name_sidebar_widgets');

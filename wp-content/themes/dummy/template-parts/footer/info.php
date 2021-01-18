b43ec990d30ce3d98a328d4a8097a93d1ad74a6f
Khoi Hoang <hello@koicha.dev>
2021-01-18 04:20:25 +0100

    Add: :sparkles: footer customization info based from Theme Customization
    Values will be added based on the WP customization we add on. if there are no values added, then it will just show 'All right Reserved' as default

--- wp-content/themes/dummy/template-parts/footer/info.php
+++ wp-content/themes/dummy/template-parts/footer/info.php
@@ -1,10 +1,18 @@
-<?php $footer_background = 'dark'; ?>
+<?php 
+    $footer_background = 'dark'; 
+    // 2nd param is the default value will be added on the site
+    $footer_customization_site_info = get_theme_mod( '_theme_name_site_info', '')
+?>
 
 <div class="c-site-info c-site-info--<?php echo $footer_background; ?>">
     <div class="o-container">
         <div class="o-row">
             <div class="o-row__column o-row__column--span12 c-site-info__text">
-                <p>All Right Reserved</p>
+                <?php if($footer_customization_site_info) :
+                echo esc_html( $footer_customization_site_info );
+                else : ?>
+                    <p>All Right Reserved</p>
+                <?php endif; ?>
             </div>
         </div>
     </div>

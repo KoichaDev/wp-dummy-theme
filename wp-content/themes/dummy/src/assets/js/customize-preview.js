b06fdf42281d137d82f398e96bbca79f9d020365
Khoi Hoang <hello@koicha.dev>
2021-01-18 05:11:13 +0100

    Add: :sparkles: Sanitization for example <b> tag
    By using JavaScript, we will sanitize by stripping out tag elements

--- wp-content/themes/dummy/src/assets/js/customize-preview.js
+++ wp-content/themes/dummy/src/assets/js/customize-preview.js
@@ -1,8 +1,9 @@
 import $ from 'jquery';
+import stripTags from './helpers/strip-tags';
 
 wp.customize('_theme_name_site_info', (value) => {
   value.bind((to) => {
     console.log(to);
-    $('.c-site-info__text').html(to);
+    $('.c-site-info__text').html(stripTags(to, '<a>'));
   });
 });

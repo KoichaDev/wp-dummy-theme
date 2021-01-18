1d4f5f8ad7f43e1707497eb1a96b01330b13d18b
Khoi Hoang <hello@koicha.dev>
2021-01-18 01:29:08 +0100

    Add: :sparkles: Markup for Menu Accesibilities

--- wp-content/themes/dummy/lib/navigation.php
+++ wp-content/themes/dummy/lib/navigation.php
@@ -10,6 +10,37 @@
 
     add_action('init', '_theme_name_register_menus');
 
+    // $attribute is the HTML attribute and 
+    // an array for the link of the anchor tag and can modify it and return it like any field
+    // ! URL: https://www.w3.org/WAI/tutorials/menus/flyout/#use-button-as-toggle
+    function _theme_name_aria_has_dropdown($attribute, $item, $args) {
+        if($args -> theme_location === 'main-menu') {
+            if(in_array('menu-item-has-children', $item -> classes)) {
+                $attribute['aria-haspopup'] = 'true';
+                $attribute['aria-expanded'] = 'false';
+            }
+            return $attribute;
+        }
+    }
+
+    add_filter('nav_menu_link_attributes', '_theme_name_aria_has_dropdown', 10 , 3);
+
+    // $dir is acronym for direction of the arrow 
+    function _theme_name_submenu_button($dir = 'down', $title) {
+        $button = '<button class="menu-button">';
+        // The span class will be used for screen reader
+        $button .= '<span class="u-screen-reader-text menu-button-show">' . 
+                        sprintf( esc_html__('Show %s submenu', '_theme_name'), $title ) . 
+                    '</span>'; 
+        $button .= '<span aria-hidden="true" class="u-screen-reader-text menu-button-hide">' . 
+                        sprintf( esc_html__('Hide %s submenu', '_theme_name'), $title ) . 
+                    '</span>';
+        $button .= ' <i class="fa fa-angle-' . $dir . '" aria-hidden="true"></i>';
+        $button .='</button>';
+        return $button;
+    }
+
+
     // $depth is how deep level nested for sub pages on navbar menu
     // $item is navigation for post object
     function _theme_name_dropdown_icon($title, $item, $args, $depth) {
@@ -19,9 +50,9 @@
             if(in_array('menu-item-has-children', $item -> classes)) {
                 // Checking if the navbar menu has zero, which means first page navbar
                 if($depth === 0) {
-                    $title .= ' <i class="fa fa-angle-down" aria-hidden="true"></i>';
+                    $title .= _theme_name_submenu_button('down', $title);
                 } else {
-                    $title .= ' <i class="fa fa-angle-right aria-hidden="true"></i>';
+                    $title .= _theme_name_submenu_button('right', $title);
                 }
             }
         }

90de294236f59c4bcbc0236edbd66693fe2cca52
Khoi <khoii87@gmail.com>
2021-02-13 13:00:01 +0100

    Add: ✨Creating A Custom Comments Template
    - Display total comments of each post
    - Check if comment exist. If exist, then display comment
    - Add Comment form to write comments on the post
    - Disable comment form if WP Admin Dashboard as it as "disabled" on the checkbox

---
+++ wp-content/themes/dummy/comments.php
@@ -0,0 +1,34 @@
+<?php if( post_password_required() ) return; ?>
+
+<div id="comments" class="c-comments">
+    <?php if( have_comments() ) : ?>
+        <h2 class="c-comments__title">
+            <!-- %1$s use these numbers if you want multiple placeholders -->
+            <?php 
+                // Translators: 1 is the number of comment and 2 is posts title 
+                printf(
+                esc_html__( _n(
+                    '%1$s Reply to "%2$s"', 
+                    '%1$s Replies to "%2$s"',
+                    get_comments_number(),
+                    '_theme_name'
+                    ) ),
+                    number_format_i18n( get_comments_number()),
+                    get_the_title() 
+                ); ?>
+        </h2>
+        <ul class="c-comments__list">
+            <?php wp_list_comments([
+                'avatar_size'       => 200,
+                'reply_text'        => 'hello'
+            ]); ?>
+        </ul>
+        <?php the_comments_pagination(); ?>
+    <?php endif; ?>
+                
+    <?php if(! comments_open() && get_comments_number() ) : ?>
+        <p class="c-comments__closed"><?php esc_html_e('Comments are closed for this post', '_theme_name'); ?></p>
+    <?php else : comment_form(); endif; ?>
+
+</div>
+
